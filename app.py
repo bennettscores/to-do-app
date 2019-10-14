3b367a15e73401a5c4a8412624cc7fc00c5d97fb
bennettscores <bennettscoresby1234@gmail.com>
2019-10-14 10:34:09 -0600

    anotherrrr

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
-from flask_heroku import heroku
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 heroku = Heroku(app)
